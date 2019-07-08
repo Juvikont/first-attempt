52093ff7154532f8ca00e8f5ddb62b450a207e8f
Yuri <y.kanapelka@gmail.com>
2019-07-08 13:19:56 +0300

    My first attempt with the Calculator

---
+++ Lesson 3/Home_Work_Calculator.py
@@ -0,0 +1,20 @@
+print('Welcome to Calculator')
+x = float(input("Please provide the first number:"))
+y = float(input("Please provide the second number:"))
+z = input("Please choose the operation: (+), (-), (*), (/)")
+if z == "+":
+    z = x + y
+    print ("=" + str(z))
+elif z == "-":
+    z = x - y
+    print ("="+str(z))
+elif z == "*":
+    if int(y) != 0:
+        print("=" + str(z))
+    else:
+     print("Division by zero!")
+elif z == "/":
+    z = x/y
+    print ("=" + str(z))
+else:
+    print ("Wrong operation!")
