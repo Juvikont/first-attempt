2d179421ff7594e875c55e3db1dc1bde82a8ccbc
Yuri <y.kanapelka@gmail.com>
2019-07-12 11:38:37 +0300

    Поправил. Теперь считает

--- Lesson 3/Calculator.py
+++ Lesson 3/Calculator.py
@@ -18,16 +18,16 @@ for i in range(len(tokens) - 1, 0, -1):
 for i in range(len(tokens) - 1, 0, -1):
     token = tokens[i]
     if token == '/':
-        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        tokens[i - 1] = tokens[i - 1] / tokens[i + 1]
         del tokens[i:i + 2]
 for i in range(len(tokens) - 1, 0, -1):
     token = tokens[i]
     if token == '+':
-        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        tokens[i - 1] = tokens[i - 1] + tokens[i + 1]
         del tokens[i:i + 2]
 for i in range(len(tokens) - 1, 0, -1):
     token = tokens[i]
     if token == '-':
-        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        tokens[i - 1] = tokens[i - 1] - tokens[i + 1]
         del tokens[i:i + 2]
 print(tokens)
