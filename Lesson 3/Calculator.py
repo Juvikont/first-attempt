3a623714596e30b46b7dff622dbbde2dec96b839
Yuri <y.kanapelka@gmail.com>
2019-07-12 10:44:10 +0300

    Привет. Подгружаю калькулятор. Пока разбираюсь в приоритетах операций. Это не конечная версия,- я работаю над ней

---
+++ Lesson 3/Calculator.py
@@ -0,0 +1,33 @@
+s = input('Expression:')
+tokens = []
+while True:
+    for i, ch in enumerate(s):
+        if ch in ('+', '-', '/', '*'):
+            tokens.append(float(s[:i]))
+            tokens.append(s[i])
+            s = s[i + 1:]
+            break
+    else:
+        tokens.append(float(s))
+        break
+for i in range(len(tokens) - 1, 0, -1):
+    token = tokens[i]
+    if token == '*':
+        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        del tokens[i:i + 2]
+for i in range(len(tokens) - 1, 0, -1):
+    token = tokens[i]
+    if token == '/':
+        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        del tokens[i:i + 2]
+for i in range(len(tokens) - 1, 0, -1):
+    token = tokens[i]
+    if token == '+':
+        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        del tokens[i:i + 2]
+for i in range(len(tokens) - 1, 0, -1):
+    token = tokens[i]
+    if token == '-':
+        tokens[i - 1] = tokens[i - 1] * tokens[i + 1]
+        del tokens[i:i + 2]
+print(tokens)
