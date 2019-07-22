4f6fe5acc21f8e49f481cf4431e4c5f698e590e6
Yuri <y.kanapelka@gmail.com>
2019-07-22 15:21:16 +0300

    Zero's task is up to check

--- Homework/Min_Max.py
+++ Homework/Min_Max.py
@@ -8,7 +8,7 @@ print(x)
 min = x[0]
 max = 0
 for index in range(len(x) - 1, 0, -1):
-    for index in range(len(x) - 1, 0, -1):
+    for index2 in range(len(x) - 1, 0, -1):
         if x[index] < min:
             min = x[index]
         elif x[index] > max:
---
+++ Homework/Zero's in the begining.py
@@ -0,0 +1,13 @@
+import random
+
+n = int(input('Please provide the number: '))
+# x = [1, 2, 5, 0, 17, 9, 0, 2, 0]
+x = []
+final = []
+for index in range(n):
+    x.append(random.randint(-1, 5))
+for index in range(len(x) - 1, 0, -1):
+    if x[index] == 0:
+        final.append(0)
+        del x[index]
+print(final + x)
