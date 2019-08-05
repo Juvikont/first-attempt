f5b5cf92ae9a9a79b6aa83835461462124e98117
Yuri <y.kanapelka@gmail.com>
2019-08-05 19:28:04 +0300

    Еще дорабатываю

--- Homework/homework 27-07-2019/Decorator.py
+++ Homework/homework 27-07-2019/Decorator.py
@@ -4,4 +4,20 @@ def reverse(func):
 
     return wrapper
 
-@wrapper
+
+def decorator(function):
+    def wrapper2(*args):
+        print(*args)
+        res = function(*args)
+        print(function.__name__)
+        function(*args)
+
+    return wrapper2
+
+
+@decorator
+def summa(a, b):
+    return a + b
+
+
+print(summa(5, 6))
